1388d57991cdf386fa3806a2fb5a485c5feb7d1c
EchoHackCmd <EchoHackCmd@gmail.com>
2020-12-21 13:11:32 +0000

    Update ClientInstance.h

--- Client Base/SDK/Classes/ClientInstance.h
+++ Client Base/SDK/Classes/ClientInstance.h
@@ -11,4 +11,19 @@ public:
 			return nullptr;
 		}
 	}
+	class MinecraftGame* MinecraftGame() {
+		if (this != nullptr) {
+			return *reinterpret_cast<class MinecraftGame**>(reinterpret_cast<__int64>(this) + 0x98);
+		}
+		else {
+			return nullptr;
+		}
+	}
+};
+
+class MinecraftGame {
+public:
+	bool canUseKeys() {
+		return *(bool*)(reinterpret_cast<uintptr_t>(this) + 0x260);
+	}
 };
