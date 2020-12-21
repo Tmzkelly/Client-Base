1ee66d0b7846d44a5d2106861a43245a58ec9708
EchoHackCmd <EchoHackCmd@gmail.com>
2020-12-21 19:42:12 +0000

    Update Jetpack.cpp

--- Client Base/Client/Modules/Jetpack.cpp
+++ Client Base/Client/Modules/Jetpack.cpp
@@ -5,10 +5,18 @@ void Jetpack::onTick() {
 	MinecraftGame* mcGame = Minecraft::ClientInstance()->MinecraftGame();
 	if (Player != nullptr && mcGame != nullptr) {
 		if (Utils::usingKey(this->key) && mcGame->canUseKeys()) {
-			Utils::DebugLogOutput("!");
 			Vec2 currRot = Player->bodyRot;
 			Vec2 angles = Vec2((currRot.x) * -(PI / 180.f), (currRot.y + 90.0f) * (PI / 180.f));
 			Player->velocity = Vec3(cos(angles.y) * cos(angles.x) * speed, sin(angles.x) * speed, sin(angles.y) * cos(angles.x) * speed);
+			/* FOV */
+			modified = true;
+			Player->setFieldOfViewModifier(255.f);
+		}
+		else {
+			if (modified) {
+				modified = false;
+				Player->setFieldOfViewModifier(1.0f);
+			}
 		}
 	}
 }
